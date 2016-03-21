a47d609d9acc37df829617c3e951c29d1f07e763
Anthony Gold and Nick Arrasate <nick@>
2016-03-21 11:21:01 -0700

    added BMI and temp conversion functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+
+/*===Calculator Functions
+====================================*/
+
 var add = function(number1, number2) {
 	return number1 + number2;
 };
@@ -15,6 +19,44 @@ var divide = function(number1, number2) {
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
+
 var number2 = parseInt(prompt("Enter another number:"));
+
 var result = divide(number1, number2);
+
 alert(result);
+
+/*===BMI Functions
+====================================*/
+
+var userBmi = function(weight,height) {
+	return (weight * 703) / (height * height);
+};
+
+var weight = parseInt(prompt("How much do you weight?"));
+
+var height = parseInt(prompt("How tall are you?"));
+
+var result = userBmi(height,weight);
+
+alert("Your BMI is " + result.toPrecision(4) + ".");
+
+/*===Temperature Functions
+====================================*/
+
+var temp = parseInt(prompt("Enter Fahrenheit to convert to Celcius"));
+
+var fToC = function(temp) {
+	return temp * (9/5) + 32
+};
+
+var cToF = function(temp) {
+	return (temp - 32) * (5/9);
+};
+
+var result = cToF(temp);
+
+alert(result.toPrecision(3));
+
+/*===Unit conversion Functions
+====================================*/
